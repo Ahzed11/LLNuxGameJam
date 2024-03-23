7acc687b9b8c1698f4f7464218aa251aa7a80fce
Richard de Jamblinne <richard.dejamblinne@student.uclouvain.be>
2024-03-23 14:21:30 +0100

    update ennemy

--- scenes/ennemy/ennemy.gd
+++ scenes/ennemy/ennemy.gd
@@ -2,14 +2,31 @@ extends CharacterBody2D
 
 
 const run_speed = 50.0
-var player
+
+@export var max_speed : float = 300
+@export var acceleration : float = 100
+@export var friction : float = 0.005
+
+var direction: Vector2
+
+@onready
+var MIN_SPEED_VECTOR := Vector2(-max_speed, -max_speed)
+@onready
+var MAX_SPEED_VECTOR := Vector2(max_speed, max_speed)
+@onready
+var player = get_node("%Player")
 
 func _ready():
-	player = get_node("%Player")
+	pass
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
-	if player:
-		velocity = position.direction_to(player.position).normalized() * run_speed
-	move_and_slide()
+	direction = position.direction_to(player.position)
+	print(direction)
+	if direction == Vector2.ZERO:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	else:
+		velocity += direction * acceleration
 	
+	velocity = velocity.clamp(MIN_SPEED_VECTOR, MAX_SPEED_VECTOR)
+	move_and_slide()
