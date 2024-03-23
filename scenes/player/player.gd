8634fabb33fcee288619532a053b97b30d04e2fc
Alexandre Zenon <a.zenon@student.uclouvain.be>
2024-03-23 13:55:36 +0100

    player movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
+@export var max_speed : float
+@export var acceleration : float
+@export var friction : float = 0.05
 
-const SPEED = 300.0
+var direction: Vector2
+
+@onready
+var MIN_SPEED_VECTOR := Vector2(-max_speed, -max_speed)
+@onready
+var MAX_SPEED_VECTOR := Vector2(max_speed, max_speed)
+
+func _process(delta):
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 
 func _physics_process(delta):
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
-	velocity = direction * SPEED
-
+	if direction == Vector2.ZERO:
+		velocity = velocity.lerp(Vector2.ZERO, friction)
+	else:
+		velocity += direction * acceleration
+	
+	velocity = velocity.clamp(MIN_SPEED_VECTOR, MAX_SPEED_VECTOR)
 	move_and_slide()
