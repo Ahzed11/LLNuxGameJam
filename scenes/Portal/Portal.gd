62b6d9cd820e36516c371c88761e697ab3cf76e6
Arno Pigeon <git@sysody.eu>
2024-03-24 11:28:39 +0100

    Particles

--- scenes/Portal/Portal.gd
+++ scenes/Portal/Portal.gd
@@ -18,12 +18,13 @@ func _on_timer_safe_timeout():
 	SignalBus.on_portail_end_of_safe.emit()
 
 func _on_timer_danger_timeout():
-
 	$Timer_Tp.start()
 	SignalBus.on_portail_end_of_danger.emit(false)
 
 func _on_timer_tp_timeout():
 	tp_counter+=1
+	%GPUParticles2D.position = position
+	%GPUParticles2D.emitting = true
 	var player_position = %Player.position
 	position = player_position + Vector2(randf_range(-3000,3000),randf_range(-3000,3000))
 	SignalBus.on_portail_tp.emit(global_position, tp_counter)
