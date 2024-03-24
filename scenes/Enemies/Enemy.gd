c2fbcce759413fa2b4b7ba3c48c72dda4d1b5420
Alexandre Zenon <a.zenon@student.uclouvain.be>
2024-03-24 09:21:13 +0100

    added scoring

--- scenes/Enemies/Enemy.gd
+++ scenes/Enemies/Enemy.gd
@@ -4,6 +4,7 @@ class_name Enemy
 const run_speed = 50.0
 @onready var explosion := preload("res://scenes/audio/explosion/explosion.tscn")
 var player
+var score_points_on_death = 50
 
 func _ready():
 	super()
@@ -24,6 +25,7 @@ func die():
 	var explosion_instance = explosion.instantiate()
 	explosion_instance.position = global_position
 	get_tree().root.add_child(explosion_instance)
+	SignalBus.on_score_gained.emit(score_points_on_death)
 	queue_free()
 
 func go_die(a:bool):
--- scenes/main_level.gd
+++ scenes/main_level.gd
@@ -1,20 +1,11 @@
 extends Node2D
 
-@export var ennemy1: PackedScene
+var total_score := 0
 
-var time = 0;
-var loc
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	SignalBus.on_score_gained.connect(update_score)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-#	time += delta
-#	if time > 1:
-#		var new_ennemy1: Enemy = ennemy1.instantiate()
-#		new_ennemy1.position = loc.position
-#		time = 0
-#		loc.progress_ratio = randf()
-#		add_child(new_ennemy1)
-	pass
+func update_score(score: int):
+	total_score += score
+	SignalBus.on_total_score_updated.emit(total_score)
