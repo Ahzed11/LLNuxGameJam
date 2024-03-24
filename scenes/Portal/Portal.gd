32394cc755c2231ca2f087c97661a58e3c330673
Richard de Jamblinne <richard.dejamblinne@student.uclouvain.be>
2024-03-24 05:54:39 +0100

    upgrade portal and fix main_level

--- scenes/Portal/Portal.gd
+++ scenes/Portal/Portal.gd
@@ -3,22 +3,19 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(position)
-	print(global_position)
-	$Timer_Safe.start(0.1)
-	SignalBus.on_portail_tp.emit(global_position)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	$Timer_Tp.start(0.1)
 
 func _on_timer_safe_timeout():
-	var player_position = %Player.position
-	position = player_position + Vector2(randf_range(-500,500),randf_range(-500,500))
-	SignalBus.on_portail_tp.emit(global_position)
-	$Timer_dead.start()
-
-func _on_timer_dead_timeout():
+	$Timer_Danger.start()
+	SignalBus.on_portail_end_of_safe.emit()
+	
+func _on_timer_danger_timeout():
+	$Timer_Tp.start()
 	get_tree().call_group("Character","in_portal",$Portal_Area)
+	SignalBus.on_portail_end_of_danger.emit()
+
+func _on_timer_tp_timeout():
+	var player_position = %Player.position
+	position = player_position + Vector2(randf_range(-5000,5000),randf_range(-5000,5000))
+	SignalBus.on_portail_tp.emit(global_position)
 	$Timer_Safe.start()
